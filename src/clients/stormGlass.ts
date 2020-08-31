753001744dc353be7c871c9785a19da725cc92b0
Renato Diniz <dinizre@hotmail.com>
2020-08-31 11:21:27 -0300

    [C02P02] - Desenvolvendo o API client - Tratamento de erros com Typescript e axios

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosStatic } from 'axios';
+import { InternalError } from '@src/util/errors/internal-error';
 
 export interface StormGlassPointSource {
     [key: string]: number;
@@ -30,6 +31,21 @@ export interface ForecastPoint {
     windSpeed: number;
 }
 
+export class ClientRequestError extends InternalError {
+    constructor(message: string) {
+        const internalMessage = `Unexpected error when trying to communicate to StormGlass`;
+        super(`${internalMessage}: ${message}`);
+    }
+}
+
+export class StormGlassResponseError extends InternalError {
+    constructor(message: string) {
+        const internalMessage =
+            'Unexpected error returned by the StormGlass service';
+        super(`${internalMessage}: ${message}`);
+    }
+}
+
 export class StormGlass {
 
     readonly stormGlassAPIParams =
@@ -52,7 +68,14 @@ export class StormGlass {
             );
             return this.normalizeResponse(response.data);
         } catch (err) {
-            throw new Error(`Unexpected error when trying to communicate to StormGlass: ${err.message}`);
+            if (err.response && err.response.status) {
+                throw new StormGlassResponseError(
+                    `Error: ${JSON.stringify(err.response.data)} Code: ${
+                    err.response.status
+                    }`
+                );
+            }
+            throw new ClientRequestError(err.message);
         }
 
     }
