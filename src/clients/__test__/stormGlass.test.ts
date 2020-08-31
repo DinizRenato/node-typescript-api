12617966d8c8d1815c57116e988a5eed0c23e5ab
Renato Diniz <dinizre@hotmail.com>
2020-08-30 22:03:33 -0300

    [C02P01-3] - 😮Como utilizar Jest com Typescript da maneira certa com Jest Mocks

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -7,13 +7,14 @@ import stormglassNormalizedResponseFixture from '@test/fixtures/stormglass_norma
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
     it('should return the normalized forecast from the StormGlass service', async () => {
         const lat = -33.792726;
         const lng = 151.289824;
 
-        axios.get = jest.fn().mockReturnValue({ data: stormglassWeather3HoursFixture });
+        mockedAxios.get.mockResolvedValue({ data: stormglassWeather3HoursFixture });
 
-        const stormGlass = new StormGlass(axios);
+        const stormGlass = new StormGlass(mockedAxios);
         const response = await stormGlass.fetchPoints(lat, lng);
         expect(response).toEqual(stormglassNormalizedResponseFixture);
     });
